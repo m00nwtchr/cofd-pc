aed594463d653e573dd54e366db85b4f39b0525a
Łukasz Mariański <lmarianski@protonmail.com>
2023-04-26 23:55:31 +0200

    add merits to scraper

--- cofd/xtask/src/bin/scraper.rs
+++ cofd/xtask/src/bin/scraper.rs
@@ -1,12 +1,15 @@
 #![feature(iter_array_chunks)]
+use std::collections::HashMap;
 use std::path::Path;
 use std::{fs, path::PathBuf};
 
 use lazy_static::lazy_static;
 use reqwest::Url;
+use ron::ser::PrettyConfig;
 use scraper::{ElementRef, Html, Selector};
 
 mod gifts;
+mod merits;
 
 lazy_static! {
 	static ref PATH: PathBuf =
@@ -16,19 +19,39 @@ lazy_static! {
 			.join("data");
 }
 
+enum PageType {
+	Gifts,
+	Merits,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let urls = ["https://codexofdarkness.com/wiki/Gifts"];
+	let urls = [
+		("https://codexofdarkness.com/wiki/Gifts", PageType::Gifts),
+		(
+			"https://codexofdarkness.com/wiki/Merits,_Universal",
+			PageType::Merits,
+		),
+		// (
+		// 	"https://codexofdarkness.com/wiki/Merits,_Vampire",
+		// 	PageType::Merits,
+		// ),
+	];
 
-	for url in &urls {
-		download(url).await?;
+	let mut handles = Vec::new();
+	for (url, page) in urls {
+		handles.push(tokio::spawn(download(url.to_string(), page)));
+	}
+
+	for handle in handles {
+		handle.await.unwrap()?;
 	}
 
 	Ok(())
 }
 
-async fn download(url: &str) -> anyhow::Result<()> {
-	let url = Url::parse(url).expect("Invalid url");
+async fn download(url: String, page: PageType) -> anyhow::Result<()> {
+	let url = Url::parse(&url).expect("Invalid url");
 
 	let page_name = url
 		.path_segments()
@@ -43,7 +66,8 @@ async fn download(url: &str) -> anyhow::Result<()> {
 		std::fs::create_dir_all(&path)?;
 	}
 
-	let html_path = path.join(format!("{page_name}.html"));
+	let name = page_name.replace(',', "");
+	let html_path = path.join(format!("{name}.html"));
 
 	let text;
 	if !html_path.exists() {
@@ -57,18 +81,18 @@ async fn download(url: &str) -> anyhow::Result<()> {
 		text = fs::read_to_string(html_path)?;
 	}
 
-	parse(&page_name, &text)?;
+	parse(&name, &text, page)?;
 
 	Ok(())
 }
 
-fn parse(page_name: &str, text: &str) -> anyhow::Result<()> {
+fn parse(page_name: &str, text: &str, page: PageType) -> anyhow::Result<()> {
 	let document = Html::parse_document(text);
 
 	let selector = Selector::parse(".mw-parser-output > section, h2").unwrap();
 	let table_sel = Selector::parse("table").unwrap();
 
-	let mut vec = Vec::new();
+	let mut map = HashMap::new();
 	for [header, section] in document.select(&selector).skip(1).array_chunks() {
 		let title = header.text().last().expect("Last text in header");
 		let table = section.select(&table_sel).next().unwrap();
@@ -81,21 +105,20 @@ fn parse(page_name: &str, text: &str) -> anyhow::Result<()> {
 			.filter_map(ElementRef::wrap)
 			.skip(1)
 		{
-			let mut vecc = Vec::new();
+			let mut vec = Vec::new();
 			for td in tr.children().filter_map(ElementRef::wrap) {
-				vecc.push(td.inner_html().trim().to_string());
+				vec.push(td.inner_html().trim().to_string());
 			}
 
-			if vecc.len() == 1 {
-				vecc.push(title.to_string());
-			}
-			vec.push(vecc);
+			map.entry(title.to_string()).or_insert(Vec::new()).push(vec);
 		}
 	}
 
-	let txt = match page_name {
-		"Gifts" => ron::ser::to_string(&gifts::parse_gifts(vec))?,
-		_ => String::new(),
+	let txt = match page {
+		PageType::Gifts => ron::ser::to_string(&gifts::parse_gifts(map))?,
+		PageType::Merits => {
+			ron::ser::to_string_pretty(&merits::parse_merits(map), PrettyConfig::default())?
+		} // _ => String::new(),
 	};
 
 	fs::write(PATH.join(format!("{}.ron", page_name.to_lowercase())), txt)?;
